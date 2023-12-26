2822792ae30dfa7dc2c30e0361b281dbd03124cd
ilhamNR <ilhamnurramadhan19@gmail.com>
2023-12-26 20:38:14 +0700

    db migration

---
+++ database/migrations/2023_12_26_130544_create_doctors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('address', 255);
+            $table->string('no_hp', 50);
+            $table->unsignedBigInteger('poli_id');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
---
+++ database/migrations/2023_12_26_131630_create_checkups_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('checkups', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('checkup_register_id');
+            $table->dateTime('checkup_date');
+            $table->text('details');
+            $table->integer('cost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('checkups');
+    }
+};
